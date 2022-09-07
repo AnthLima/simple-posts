a48a59d7a1b295c906576ed33baa45241aca6f25
AnthLima <tryout3699@gmail.com>
2022-09-07 17:42:38 -0300

    feat: Add header component and import css global

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-
+// import App from './App'
+import { Header } from './components/Header'  
+import './global.css';
 import {Post} from './Post'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Header/>
     {/* <App /> */}
-    <Post 
+    {/* <Post 
       author={'Anthony'} 
       description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo maiores amet unde placeat cupiditate earum eos, harum nemo veritatis nulla. Adipisci impedit rem molestias provident maiores non! Perspiciatis, commodi dicta.'}
     />
     <Post 
       author={'Diego'} 
       description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo maiores amet unde placeat cupiditate earum eos, harum nemo veritatis nulla. Adipisci impedit rem molestias provident maiores non! Perspiciatis, commodi dicta.'}
-    />
+    /> */}
 
   </React.StrictMode>
 )
